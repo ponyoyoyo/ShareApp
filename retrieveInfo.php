a3d092e03f661965e64d58893d2611f4946a4808
shelleyyap <shelley_yap@hotmail.com>
2016-03-29 10:53:27 +0800

    [WIP] Edit button for Shared Items table
    Still need to handle process.php

--- retrieveInfo.php
+++ retrieveInfo.php
@@ -67,7 +67,7 @@
 				</table>
 			</div>
 		<?php 
-	        $query = "SELECT i.type, i.itemid, i.feeflag, m.name,  i.itemname, pickuplocation, returnlocation FROM item i, loan l, member m WHERE i.email = '{$_SESSION['email']}' AND l.borrower = m.email AND l.lender = i.email"; 
+	        $query = "SELECT i.type, i.itemid, i.feeflag, m.name,  i.itemname, pickuplocation, returnlocation FROM item i, loan l, member m WHERE i.email = '{$_SESSION['email']}' AND i.itemid = l.itemid  AND l.borrower = m.email AND l.lender = i.email"; 
 	        //$dbconn->prepare($query);
 	        $result = pg_query($query); 
 			$i = 0;
@@ -111,7 +111,7 @@
 					next($row);
 					$y = $y + 1;
 				}
-				echo '<td><p data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" ><span class="glyphicon glyphicon-pencil"></span></button></p></td>
+				echo '<td><p data-placement="top" data-toggle="tooltip" title="Edit"><a type="button" href="#edit1' . $row[1] . '" class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal"><span class="glyphicon glyphicon-pencil"></span></button></p></td>
 	    			<td><p data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></p></td>';
 				echo '</tr>';
 				$i = $i + 1;
@@ -135,6 +135,7 @@
 		<a href="logout.php" class="btn btn-danger" role="button">Logout</a>
 	</div>
 	                
+	<!-- Edit Button in Prompt for Your Items table-->    
 	<?php
 		$query = "SELECT id FROM item";
 		$result = pg_query($query);
@@ -161,71 +162,72 @@
 				      			</div>
 
 				          		<form action = "process.php" method="post" role="form" >
-				          		<div class="modal-body">
-				          			<div class="form-group">
-				          				<input class="form-control "type="text" name="name" id="name" tabindex="1" placeholder="Item name" value="' . $name . '">
-				        			</div>
-				        		
-				        			<div class="form-group">
-										<select class="form-control" name="type">';
-											if ($type == "furnitures"){
-											
-												echo '<option value="furnitures" selected>Furniture</option>
-												<option value="tools">Tools</option>
-												<option value="appliances">Appliances</option>
-												<option value="books">Books</option>';
-											} else if ($type == "tools"){
-											
-												echo '<option value="furnitures">Furniture</option>
-												<option value="tools" selected>Tools</option>
-												<option value="appliances">Appliances</option>
-												<option value="books">Books</option>';
-											} else if ($type == "appliances"){
-											
-												echo '<option value="furnitures">Furniture</option>
-												<option value="tools">Tools</option>
-												<option value="appliances" selected>Appliances</option>
-												<option value="books">Books</option>';
-											} else if ($type == "books"){
-											
-												echo '<option value="furnitures">Furniture</option>
-												<option value="tools">Tools</option>
-												<option value="appliances">Appliances</option>
-												<option value="books" selected>Books</option>';
-											}  
-										echo '</select>
-				        			</div>
-				        			
-				        			<div class="form-group">
-				        				<input type="text" name="pickup" id="pickup" tabindex="2" class="form-control" placeholder="Pick Up Location" value="' . $pickup .'">
-				        			</div>
+					          		<div class="modal-body">
+					          			<div class="form-group">
+					          				<input class="form-control "type="text" name="name" id="name" tabindex="1" placeholder="Item name" value="' . $name . '">
+					        			</div>
+					        		
+					        			<div class="form-group">
+											<select class="form-control" name="type">';
+												if ($type == "furnitures"){
+												
+													echo '<option value="furnitures" selected>Furniture</option>
+													<option value="tools">Tools</option>
+													<option value="appliances">Appliances</option>
+													<option value="books">Books</option>';
+												} else if ($type == "tools"){
+												
+													echo '<option value="furnitures">Furniture</option>
+													<option value="tools" selected>Tools</option>
+													<option value="appliances">Appliances</option>
+													<option value="books">Books</option>';
+												} else if ($type == "appliances"){
+												
+													echo '<option value="furnitures">Furniture</option>
+													<option value="tools">Tools</option>
+													<option value="appliances" selected>Appliances</option>
+													<option value="books">Books</option>';
+												} else if ($type == "books"){
+												
+													echo '<option value="furnitures">Furniture</option>
+													<option value="tools">Tools</option>
+													<option value="appliances">Appliances</option>
+													<option value="books" selected>Books</option>';
+												}  
+											echo '</select>
+					        			</div>
+					        			
+					        			<div class="form-group">
+					        				<input type="text" name="pickup" id="pickup" tabindex="2" class="form-control" placeholder="Pick Up Location" value="' . $pickup .'">
+					        			</div>
 
-				        			<div class="form-group">
-				        				<input type="text" name="return" id="return" tabindex="2" class="form-control" placeholder="Return Location" value="' . $return . '">
-				        			</div>
-				        			
-				        			<div class="form-group">
-				        				<input type="date" name="availableDate" id="availableDate" tabindex="2" class="form-control" placeholder="Available Date dd/mm/yy" value="'. $date .'">
-				        			</div>
-				        			
-				        			<div class="form-group">
-				        				<input type="text" name="description" id="description" tabindex="2" class="form-control" placeholder="Description"value="' . $description . '">
-				        			</div>
-				        			
-				        			<div class="form-group">';
-				        				if ($fee == 1) {
-				        					echo '<input type="checkbox" class="" name="fee" id="fee" checked value="1"><label for="fee">Fee Flag</label>';
-				        				} else {
-				        					echo '<input type="checkbox" class="" name="fee" id="fee"><label for="fee">Fee Flag</label>';
-				        				}
-				        			echo '</div>
-				        			
-				        			<input type="hidden" name="id" id="id" value="' . $id . '">
-				      			</div>
-				          
-								<div class="modal-footer ">
-									<button type="submit" name="edit-submit" id="edit-submit" class="btn btn-warning btn-lg" style="width: 100%;"><span class="glyphicon glyphicon-ok-sign"></span> Update</button>
-								</div>
+					        			<div class="form-group">
+					        				<input type="text" name="return" id="return" tabindex="2" class="form-control" placeholder="Return Location" value="' . $return . '">
+					        			</div>
+					        			
+					        			<div class="form-group">
+					        				<input type="date" name="availableDate" id="availableDate" tabindex="2" class="form-control" placeholder="Available Date dd/mm/yy" value="'. $date .'">       
+					        			</div>
+					        			
+					        			<div class="form-group">
+					        				<input type="text" name="description" id="description" tabindex="2" class="form-control" placeholder="Description"value="' . $description . '">
+					        			</div>
+					        			
+					        			<div class="form-group">';
+					        				if ($fee == 1) {
+					        					echo '<input type="checkbox" class="" name="fee" id="fee" checked value="1"><label for="fee">Fee Flag</label>';
+					        				} else {
+					        					echo '<input type="checkbox" class="" name="fee" id="fee"><label for="fee">Fee Flag</label>';
+					        				}
+					        			echo '</div>
+					        			
+					        			<input type="hidden" name="id" id="id" value="' . $id . '">
+					      			</div>
+					          
+									<div class="modal-footer ">
+										<button type="submit" name="edit-submit" id="edit-submit" class="btn btn-warning btn-lg" style="width: 100%;"><span class="glyphicon glyphicon-ok-sign"></span> Update</button>
+									</div>
+								</form>
 				        	</div>
 				    		<!-- /.modal-content --> 
 				  		</div>
@@ -269,6 +271,59 @@
 		}
 	?>
 
+	<!-- Edit Button in Prompt for Shared Items table -->
+	<?php
+		$query = "SELECT i.id, i.type, i.fee, i.name, i.pickup, i.return FROM item i, loan l, member m WHERE i.email = '{$_SESSION['email']}' AND i.id = l.id AND l.borrower = m.email AND l.lender = i.email"; 
+		$result = pg_query($query);
+		while ($row = pg_fetch_row($result)) {
+			$id = $row[0];
+			$type = $row[1];
+			$fee = $row[2];
+			$name = $row[3];
+			$pickup = $row[4];
+			$return = $row[5];
+			echo '<div class="modal fade" id="edit1' . $id . '" tabindex="-1" role="dialog" aria-labelledby="edit" aria-hidden="true">
+				      <div class="modal-dialog">
+				    		<div class="modal-content">
+				          		<div class="modal-header">
+				        			<button type="button" class="close" data-dismiss="modal" aria-hidden="true"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button>
+				        			<h4 class="modal-title custom_align" id="Heading">Edit Your Item Details</h4>
+				      			</div>
+				          		<form action="process.php" method="post">
+					          		<div class="modal-body">
+					          			<div class="form-group">
+					       					<input class="form-control " type="text" placeholder="Type" readonly value="' . $type . '">
+					        			</div>
+					        		
+					        			<div class="form-group">
+					        				<input class="form-control " type="text" placeholder="Fee" readonly value="' . $fee . '">
+					        			</div>
+					        			
+					        			<div class="form-group">
+					        				<input class="form-control " type="text" placeholder="Item Name" readonly value="' . $name . '">
+					        			</div>
+
+					        			<div class="form-group">
+					        				<input class="form-control " type="text" placeholder="Pick Up Location" value="' . $pickup . '">
+					        			</div>
+					        			
+					        			<div class="form-group">
+					        				<input class="form-control " type="text" placeholder="Return Location" value="' . $return . '">  
+					        			</div>
+					      			</div>
+					        		<input type="hidden" name="id" id="id" value="' . $id . '">
+					          
+									<div class="modal-footer ">
+										<button type="submit" name="edit-submit1" id="edit-submit1" class="btn btn-warning btn-lg" style="width: 100%;"><span class="glyphicon glyphicon-ok-sign"></span> Update</button>
+									</div>
+								</form>
+				        	</div>
+				    		<!-- /.modal-content --> 
+				  		</div>
+				      	<!-- /.modal-dialog --> 
+					</div>';
+		}
+	?>
 	<script type="text/javascript" src="js/jquery.min.js"></script>
 	<script type="text/javascript" src="js/bootstrap.min.js"></script>
 	<script type="text/javascript" src="js/jquery-2.1.0.min.js"></script>
