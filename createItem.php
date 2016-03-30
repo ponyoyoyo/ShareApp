b44f3eeca1584a0362ea74b6dd26e384d63a7a67
Edward <angertrail@hotmail.com>
2016-03-30 23:10:50 +0800

    Added more input boxes

--- createItem.php
+++ createItem.php
@@ -37,22 +37,48 @@
 			<div class="form-group">
 				<input type="text" name="itemName" id="itemName" class="form-control" placeholder="Item Name" value="">
 			</div>
-			<div class="form-group">
-				<select name="itemType">
+			<div class="form-group row">
+				<label> Type of Item: </label>
+				<select name="itemType" required>
 					<option value="appliances">Appliance</option>
 					<option value="tools">Tool</option>
 					<option value="furniture">Furniture</option>
 					<option value="books">Books</option>
+					<option value="others">Others</option>
+				</select>
+				<label> Need to Pay: </label>
+				<select name="fee" required>
+					<option value=1>Yes</option>
+					<option value=0>No</option>
 				</select>
 			</div>
 			<div class="form-group">
 				<label for="desc">Description</label>
-				<textarea name="desc" id="desc" form="createForm" maxlength="100" placeholder="No more than 100 characters"></textarea>
+				<textarea name="itemDesc" id="itemDesc" form="createForm" maxlength="100" placeholder="No more than 100 characters"></textarea>
+			</div>
+			<div class="form-group">
+				<input type="file" name="imagefile" accept="image/*" id="imagefile">
+			</div>
+			<div class="form-group">
+				<input type="submit" name="itemSubmit" id="itemSubmit" class="form-control btn btn-success" value="Submit">
 			</div>
 			</form>'
 			
 		if (isset($_POST[Submit])) {
+			$itemName = $_POST['itemName'];
 			$itemType = $_POST['itemType'];
+			$feeFlag = $_POST['fee'];
+			$itemDesc = $_POST['itemDesc'];
+			$itemImg = $_POST['imagefile'];
+			
 		}
+		
+		$query = 'INSERT INTO item (email, type, itemID, feeFlag, itemName, pickupLocation, returnLocation, availableDate, description, availabilityFlag)
+					VALUES ()';
+		if (pg_result_status($query) = PGSQL_FATAL_ERROR) {
+			
+		}
+		
+	?>	
 			
 </body>
